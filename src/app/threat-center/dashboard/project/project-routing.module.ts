95fce1cf9a095cfe4ba28d7c8b2397b9e7591d8a
Gaurav Thakkar <gthakkar.it@gmail.com>
2020-12-02 19:49:09 +0530

    https://github.com/threatrix/product/issues/4

--- src/app/threat-center/dashboard/project/project-routing.module.ts
+++ src/app/threat-center/dashboard/project/project-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
     path: '',
     children: [
       {
-        path: ':projectId',
+        path: '',
         component: ProjectComponent,
         resolve: {
           project: GetProjectData,
