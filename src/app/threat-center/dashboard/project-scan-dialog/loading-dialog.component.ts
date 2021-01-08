9f4720531b3d7016baed97bb0af881456c29a76c
Gaurav Thakkar <gthakkar.it@gmail.com>
2021-01-08 14:27:53 +0530

    https://github.com/threatrix/product/issues/184

--- src/app/threat-center/dashboard/project-scan-dialog/loading-dialog.component.ts
+++ src/app/threat-center/dashboard/project-scan-dialog/loading-dialog.component.ts
@@ -23,52 +23,25 @@ export class LoadingDialogComponent implements OnInit, OnDestroy {
 
     successToasterTime: number = 5;
     errorToasterTime: number = 5;
+
+    startTimerToCloseModel: Date | any = null;
     constructor
         (
             private scanHelperService: ScanHelperService,
             public activeModal: NgbActiveModal,
-            private route: ActivatedRoute,
-            private router: Router,
     ) {
 
         const source = interval(100);
         this.subscription = source.subscribe(d => {
             this.projectScanResults = this.scanHelperService.projectScanResults;
             this.recentlyScanCompleted = this.scanHelperService.recentlyScanCompleted;
-
             this.errorScanProject = this.scanHelperService.errorScanProject;
+            this.closeModelIfNoScanInProgress();
             if (this.projectScanResults.length == 0 && this.recentlyScanCompleted.length == 0 && this.errorScanProject.length == 0) {
                 this.closeModel();
             }
+
         });
-
-        // this.scanHelperService.projectScanloadingStatusObservable$
-        //     .subscribe(x => {
-        //         const lastSegOfUrl = this.router.url.substring(this.router.url.lastIndexOf('/') + 1);
-        //         if (!!x) {
-        //             this.statusMessageFromback = x['message'];
-        //             if (this.statusMessageFromback === 'ERROR') {
-        //                 // If ANY error from server/api side.
-        //                 this.isError = true;
-        //                 this.statusMessageFromback = "Something went wrong!";
-        //             } else {
-        //                 // IF NO ERROR FROM API SIDE.
-        //                 this.isError = false;
-        //                 this.projectId = x['projectId'];
-        //                 if (!!lastSegOfUrl && !!x['projectId'] && lastSegOfUrl === x['projectId'] &&
-        //                     x['message'] === 'COMPLETE') {
-        //                     // CLOSE DIALOG BOX THEN GET LATEST SCAN AND HIGHLTED IN PROJECT SCREEN.
-        //                     this.scanHelperService.updateIsHighlightNewScan(true);
-        //                     this.activeModal.dismiss();
-        //                 } else {
-        //                     // CHANGE MESSAGE AND GO TO LINK FOR THE PROJECT PAGE.
-        //                     this.scanHelperService.updateIsHighlightNewScan(false);
-        //                     // think rest of the things will handle by html
-        //                 }
-        //             }
-        //         }
-        //     });
-
     }
 
     filter(items): Array<any> {
@@ -101,6 +74,23 @@ export class LoadingDialogComponent implements OnInit, OnDestroy {
 
     gotoProject(project) {
         this.scanHelperService.gotoProjectAndUpdateRecentScan(project);
+    }
+
+    closeModelIfNoScanInProgress() {
+        if (this.projectScanResults.length == 0) {
+            if (!this.startTimerToCloseModel) {
+                this.startTimerToCloseModel = new Date();
+            }
+        } else {
+            this.startTimerToCloseModel = undefined;
+        }
+        const cDate: any = new Date();
+        if (!!this.startTimerToCloseModel) {
+            console.log((cDate - this.startTimerToCloseModel) / 1000 > 10);
+            if ((cDate - this.startTimerToCloseModel) / 1000 > 10) {
+                this.closeModel();
+            }
+        }
     }
 
     closeModel() {
