29015d4cc2eddf756cf6be85e0cd02fe7f6a9bd5
Gaurav Thakkar <gthakkar.it@gmail.com>
2021-01-28 12:05:59 +0530

    https://github.com/threatrix/product/issues/310

--- src/app/threat-center/dashboard/entity/entity.component.ts
+++ src/app/threat-center/dashboard/entity/entity.component.ts
@@ -192,37 +192,67 @@ export class EntityComponent implements OnInit {
         this.assetChart.series = [];
         this.sourceCodeLeakChart.series = [];
 
+        // // set vulnerabilityChart data
+        // this.vulnerabilityChart.series.push({ name: 'Critical', data: critical });
+        // this.vulnerabilityChart.series.push({ name: 'High', data: high });
+        // this.vulnerabilityChart.series.push({ name: 'Medium', data: medium });
+        
+        // this.vulnerabilityChart.series.push({ name: 'Low', data: low });
+        // this.vulnerabilityChart.series.push({ name: 'Info', data: info });
+
+        // // set licenseChart data
+        // this.licenseChart.series.push({ name: 'CL Strong', data: copyleftStrong });
+        // this.licenseChart.series.push({ name: 'CL Weak', data: copyleftWeak });
+        // this.licenseChart.series.push({ name: 'CL Partial', data: copyleftPartial });
+        // this.licenseChart.series.push({ name: 'CL Limited', data: copyleftLimited });
+        // this.licenseChart.series.push({ name: 'Copyleft', data: copyleft });
+        // this.licenseChart.series.push({ name: 'Custom', data: custom });
+        // this.licenseChart.series.push({ name: 'Dual', data: dual });
+        // this.licenseChart.series.push({ name: 'Permissive', data: permissive });
+
+        // // set componentChart data
+        // this.componentChart.series.push({ name: 'Not Latest', data: notLatest });
+        // this.componentChart.series.push({ name: 'Vulnerabilities', data: vulnerabilities });
+        // this.componentChart.series.push({ name: 'Risky Licenses', data: riskyLicenses });
+
+        // // set assetChart data
+        // this.assetChart.series.push({ name: 'Analyzed', data: analyzed });
+        // this.assetChart.series.push({ name: 'Skipped', data: skipped });
+        // this.assetChart.series.push({ name: 'Embedded', data: embedded });
+
+        // // set source code Leak data
+        // this.sourceCodeLeakChart.series.push({ name: 'Partial Asset Leaks', data: partialAsset });
+        // this.sourceCodeLeakChart.series.push({ name: 'Asset Leaks', data: asset })
+        // this.sourceCodeLeakChart.series.push({ name: 'Project Leaks', data: projectAsset });
+
+
+
         // set vulnerabilityChart data
-        this.vulnerabilityChart.series.push({ name: 'Critical', data: critical });
-        this.vulnerabilityChart.series.push({ name: 'High', data: high });
-        this.vulnerabilityChart.series.push({ name: 'Medium', data: medium });
-        this.vulnerabilityChart.series.push({ name: 'Low', data: low });
-        this.vulnerabilityChart.series.push({ name: 'Info', data: info });
+      this.vulnerabilityChart.series.push({ name: 'Critical', data: critical, colorClass: "red", hover: false });
+      this.vulnerabilityChart.series.push({ name: 'High', data: high, colorClass: "orange", hover: false });
+      this.vulnerabilityChart.series.push({ name: 'Medium', data: medium, colorClass: "yellow", hover: false });
+      this.vulnerabilityChart.series.push({ name: 'Low', data: low, colorClass: "lgt-blue", hover: false });
+      this.vulnerabilityChart.series.push({ name: 'Info', data: info, colorClass: "green", hover: false });
 
-        // set licenseChart data
-        this.licenseChart.series.push({ name: 'CL Strong', data: copyleftStrong });
-        this.licenseChart.series.push({ name: 'CL Weak', data: copyleftWeak });
-        this.licenseChart.series.push({ name: 'CL Partial', data: copyleftPartial });
-        this.licenseChart.series.push({ name: 'CL Limited', data: copyleftLimited });
-        this.licenseChart.series.push({ name: 'Copyleft', data: copyleft });
-        this.licenseChart.series.push({ name: 'Custom', data: custom });
-        this.licenseChart.series.push({ name: 'Dual', data: dual });
-        this.licenseChart.series.push({ name: 'Permissive', data: permissive });
+      // set licenseChart data
+      this.licenseChart.series.push({ name: 'CL Strong', data: copyleftStrong, colorClass: "red", hover: false });
+      this.licenseChart.series.push({ name: 'CL Weak', data: copyleftWeak, colorClass: "orange", hover: false });
+      this.licenseChart.series.push({ name: 'CL Partial', data: copyleftPartial, colorClass: "yellow", hover: false });
+      this.licenseChart.series.push({ name: 'CL Limited', data: copyleftLimited, colorClass: "lgt-blue", hover: false });
+      this.licenseChart.series.push({ name: 'Copyleft', data: copyleft, colorClass: "green", hover: false });
+      this.licenseChart.series.push({ name: 'Custom', data: custom, colorClass: "pink", hover: false });
+      this.licenseChart.series.push({ name: 'Dual', data: dual, colorClass: "white", hover: false });
+      this.licenseChart.series.push({ name: 'Permissive', data: permissive, colorClass: "blue", hover: false });
 
-        // set componentChart data
-        this.componentChart.series.push({ name: 'Not Latest', data: notLatest });
-        this.componentChart.series.push({ name: 'Vulnerabilities', data: vulnerabilities });
-        this.componentChart.series.push({ name: 'Risky Licenses', data: riskyLicenses });
+      // set componentChart data
+      this.componentChart.series.push({ name: 'Not Latest', data: notLatest, colorClass: "red", hover: false });
+      this.componentChart.series.push({ name: 'Vulnerabilities', data: vulnerabilities, colorClass: "orange", hover: false });
+      this.componentChart.series.push({ name: 'Risky Licenses', data: riskyLicenses, colorClass: "yellow", hover: false });
 
-        // set assetChart data
-        this.assetChart.series.push({ name: 'Analyzed', data: analyzed });
-        this.assetChart.series.push({ name: 'Skipped', data: skipped });
-        this.assetChart.series.push({ name: 'Embedded', data: embedded });
-
-        // set source code Leak data
-        this.sourceCodeLeakChart.series.push({ name: 'Partial Asset Leaks', data: partialAsset });
-        this.sourceCodeLeakChart.series.push({ name: 'Asset Leaks', data: asset })
-        this.sourceCodeLeakChart.series.push({ name: 'Project Leaks', data: projectAsset });
+      // set assetChart data
+      this.assetChart.series.push({ name: 'Analyzed', data: analyzed, colorClass: "green", hover: false });
+      this.assetChart.series.push({ name: 'Skipped', data: skipped, colorClass: "lgt-blue", hover: false });
+      this.assetChart.series.push({ name: 'Embedded', data: embedded, colorClass: "yellow", hover: false });
 
         // set categories on bar charts
         this.xaxis.categories = categories;
@@ -301,6 +331,7 @@ export class EntityComponent implements OnInit {
     },
   };
   legend = {
+    show:false,
     position: 'top',
     horizontalAlign: 'center',
     offsetX: 0,
@@ -337,7 +368,32 @@ export class EntityComponent implements OnInit {
     stroke: this.stroke,
     colors: ['#ff2b2b', '#ff5252', '#ffa21d', '#00acc1', '#00e396'],//,'#11c15b'
     series: [],
-    xaxis: this.xaxis
+    xaxis: this.xaxis,
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        let str = "";
+        for (let i = 0; i < w.config.series.length; i++) {
+          let data = 0;
+          if (!!w.config.series[i].data && w.config.series[i].data.length >= 1)
+            data = w.config.series[i].data[dataPointIndex];
+          str += "<li class='" + w.config.series[i].colorClass + "'>" + w.config.series[i].name + "(" + data + ")</li>";
+        }
+        let orgData = 0;
+        if (!!w.config.series[seriesIndex].data && w.config.series[seriesIndex].data.length >= 1) {
+          orgData = w.config.series[seriesIndex].data[dataPointIndex];
+        }
+        return (
+          '<div class=" arrow_box chart-overlay">' +
+          "<span class='active-fig'>" +
+          w.config.series[seriesIndex].name +
+          ": " +
+          orgData +
+          "</span>" +
+          "<ul class='chart-all-lgnd'>" + str + "</ul>" +
+          "</div>"
+        );
+      }
+    }
   };
   public licenseChart = {
     chart: this.chart,
@@ -345,9 +401,35 @@ export class EntityComponent implements OnInit {
     legend: this.legend,
     dataLabels: this.dataLabels,
     stroke: this.stroke,
-    colors: ['#ff2b2b', '#ff5252', '#ffa21d', '#11c15b'],
+    // colors: ['#ff2b2b', '#ff5252', '#ffa21d', '#11c15b'],
+    colors: ['#ff2b2b', '#ff5252', '#ffa21d', '#00acc1', '#00e396', '#c71585', '#f8f8ff', '#4680ff'],
     series: [],
-    xaxis: this.xaxis
+    xaxis: this.xaxis,
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        let str = "";
+        for (let i = 0; i < w.config.series.length; i++) {
+          let data = 0;
+          if (!!w.config.series[i].data && w.config.series[i].data.length >= 1)
+            data = w.config.series[i].data[dataPointIndex];
+          str += "<li class='" + w.config.series[i].colorClass + "'>" + w.config.series[i].name + "(" + data + ")</li>";
+        }
+        let orgData = 0;
+        if (!!w.config.series[seriesIndex].data && w.config.series[seriesIndex].data.length >= 1) {
+          orgData = w.config.series[seriesIndex].data[dataPointIndex];
+        }
+        return (
+          '<div class=" arrow_box chart-overlay">' +
+          "<span class='active-fig'>" +
+          w.config.series[seriesIndex].name +
+          ": " +
+          orgData +
+          "</span>" +
+          "<ul class='chart-all-lgnd'>" + str + "</ul>" +
+          "</div>"
+        );
+      }
+    }
   };
   public componentChart = {
     chart: this.chart,
@@ -357,7 +439,32 @@ export class EntityComponent implements OnInit {
     stroke: this.stroke,
     colors: ['#ff2b2b', '#ff5252', '#ffa21d', '#00acc1', '#00e396'],//,'#11c15b'
     series: [],
-    xaxis: this.xaxis
+    xaxis: this.xaxis,
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        let str = "";
+        for (let i = 0; i < w.config.series.length; i++) {
+          let data = 0;
+          if (!!w.config.series[i].data && w.config.series[i].data.length >= 1)
+            data = w.config.series[i].data[dataPointIndex];
+          str += "<li class='" + w.config.series[i].colorClass + "'>" + w.config.series[i].name + "(" + data + ")</li>";
+        }
+        let orgData = 0;
+        if (!!w.config.series[seriesIndex].data && w.config.series[seriesIndex].data.length >= 1) {
+          orgData = w.config.series[seriesIndex].data[dataPointIndex];
+        }
+        return (
+          '<div class=" arrow_box chart-overlay">' +
+          "<span class='active-fig'>" +
+          w.config.series[seriesIndex].name +
+          ": " +
+          orgData +
+          "</span>" +
+          "<ul class='chart-all-lgnd'>" + str + "</ul>" +
+          "</div>"
+        );
+      }
+    }
   };
   public assetChart = {
     chart: this.chart,
@@ -367,7 +474,32 @@ export class EntityComponent implements OnInit {
     stroke: this.stroke,
     colors: ['#11c15b', '#4680ff', '#ffa21d'],
     series: [],
-    xaxis: this.xaxis
+    xaxis: this.xaxis,
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        let str = "";
+        for (let i = 0; i < w.config.series.length; i++) {
+          let data = 0;
+          if (!!w.config.series[i].data && w.config.series[i].data.length >= 1)
+            data = w.config.series[i].data[dataPointIndex];
+          str += "<li class='" + w.config.series[i].colorClass + "'>" + w.config.series[i].name + "(" + data + ")</li>";
+        }
+        let orgData = 0;
+        if (!!w.config.series[seriesIndex].data && w.config.series[seriesIndex].data.length >= 1) {
+          orgData = w.config.series[seriesIndex].data[dataPointIndex];
+        }
+        return (
+          '<div class=" arrow_box chart-overlay">' +
+          "<span class='active-fig'>" +
+          w.config.series[seriesIndex].name +
+          ": " +
+          orgData +
+          "</span>" +
+          "<ul class='chart-all-lgnd'>" + str + "</ul>" +
+          "</div>"
+        );
+      }
+    }
   };
   public sourceCodeLeakChart = {
     chart: this.chart,
@@ -379,4 +511,13 @@ export class EntityComponent implements OnInit {
     series: [],
     xaxis: this.xaxis
   };
+
+  getAdditionData(data) {
+    if (!!data && data.length >= 1) {
+      // return data[0];
+      return data.reduce((prev, next) => prev + (+next), 0);
+    } else {
+      return 0;
+    }
+  }
 }
