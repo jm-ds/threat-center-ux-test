88979087ed1afdf44a1713b1270b88d4ced2a26e
Gaurav Thakkar <gthakkar.it@gmail.com>
2021-02-09 11:40:25 +0530

    https://github.com/threatrix/product/issues/291

--- src/app/core/services/core-helper.service.ts
+++ src/app/core/services/core-helper.service.ts
@@ -266,6 +266,21 @@ export class CoreHelperService {
     getBrowserBackButton() {
         return this.isBrowserBackclick;
     }
+
+    getDifferencebetweenStrings(a: string, b: string) {
+        var i = 0;
+        var j = 0;
+        var result = "";
+    
+        while (j < b.length) {
+          if (a[i] != b[j] || i == a.length)
+            result += b[j];
+          else
+            i++;
+          j++;
+        }
+        return result;
+      }
 }
 
 
