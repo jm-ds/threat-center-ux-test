93cb8cbbc150eb9508e79cb1fe25b3425c07a5bf
Gaurav Thakkar <gaurav.thakkar@prospecta.com>
2021-03-19 09:32:38 +0530

    https://github.com/threatrix/product/issues/342

---
+++ src/app/models/entity.ts
@@ -0,0 +1,41 @@
+import { PageInfo } from "./common";
+import { License } from "./license";
+import { EntityMetricsGroup, EntityMetricsSummaryGroup } from "./metrics";
+import { ProjectConnection } from "./project";
+import { Vulnerability } from "./vulnerability";
+
+export class Entity {
+    entityId: string;
+    parentEntityId: string;
+    name: string;
+    projects: ProjectConnection;
+    entityMetricsGroup: EntityMetricsGroup;
+    entityMetricsSummaryGroup: EntityMetricsSummaryGroup;
+    entityComponents: any;
+    vulnerabilities: Vulnerability[];
+    licenses: License[];
+    childEntities: Entity[];
+}
+
+export class EntityConnection {
+    edges: EntityEdge[];
+    pageInfo: PageInfo;
+    totalCount: number;
+}
+
+export class EntityEdge {
+    node: Entity;
+    cursor: string;
+    constructor(node: Entity, cursor: string) {
+        this.node = node;
+        this.cursor = cursor;
+    }
+}
+
+export type EntityQuery = {
+    entity: Entity;
+}
+
+export type EntityListQuery = {
+    entities: EntityConnection;
+}
