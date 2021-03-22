2926d12582cb9fd6a040b855cc8800d11a01bbba
udv <udv0412@gmail.com>
2021-03-22 19:00:38 +0300

    Invite users https://github.com/threatrix/product/issues/219

--- src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { InviteService } from '@app/admin/services/invite.service';
 import { CommonUIMethodsDecorator } from '@app/core/decorators/common.decorator';
 import { CoreHelperService } from '@app/core/services/core-helper.service';
 import { AuthenticationService } from '@app/security/services';
-import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -18,14 +19,31 @@ import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 export class NavRightComponent implements OnInit {
 
+  inviteUrl: string;
+
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router,
-    public coreHelperService: CoreHelperService) { }
+    public coreHelperService: CoreHelperService,
+    private inviteService: InviteService) { }
 
   ngOnInit() { }
 
   logout() {
     this.coreHelperService.logoutUser();
   }
+
+  inviteUser(inviteUrlDialog) {
+    this.inviteService.createInvite().subscribe(
+      data => {
+          let inviteHash = data.data.createInvite.inviteHash;
+          const link = '/admin/invite/show/'+ inviteHash;
+                this.router.navigate([link]);
+      },
+      error => {
+          console.error("NavRightComponent", error);
+      }
+  );
+  }
+
 }
