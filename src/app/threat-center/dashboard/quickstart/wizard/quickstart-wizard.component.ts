238e098c5740cebbc909b8cb8d263a6fde343d81
Gaurav Thakkar <gaurav.thakkar@prospecta.com>
2021-04-13 11:24:13 +0530

    https://github.com/threatrix/product/issues/338

--- src/app/threat-center/dashboard/quickstart/wizard/quickstart-wizard.component.ts
+++ src/app/threat-center/dashboard/quickstart/wizard/quickstart-wizard.component.ts
@@ -255,6 +255,7 @@ export class QuickstartWizardComponent implements OnInit, OnDestroy {
     }
 
     onRowSelect(event) {
+        this.selectedItem = '';
         const selectRepo = this.selectedRepos[0];
         if (!!selectRepo) {
             if (!!selectRepo.node.defaultBranchRef && !!selectRepo.node.defaultBranchRef.name) {
