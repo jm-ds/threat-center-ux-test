f0c6660e5aa6d78745f162f5b4d8dd35de2a268a
Andrey Degtiarev <adegtiarev@gmail.com>
2021-07-23 14:55:10 +0400

    Vulnerability detail add Affected components https://github.com/threatrix/product/issues/644

--- src/app/threat-center/dashboard/project/scanasset/scanasset-detail/scanasset-detail.component.ts
+++ src/app/threat-center/dashboard/project/scanasset/scanasset-detail/scanasset-detail.component.ts
@@ -128,28 +128,14 @@ export class ScanAssetDetailComponent implements OnInit {
     this.stateService.component_tabs_selectedTab = $event.nextId;
   }
 
-  public releaseCols = ['Name', 'Version'];
-  public releases = [
-    { version: 'v1.4.3-RELEASE', date: '05/20/2020' },
-    { version: 'v1.4.2-RELEASE', date: '05/01/2020' },
-    { version: 'v1.4.1-RELEASE', date: '04/13/2020' },
-    { version: 'v1.4.0-RELEASE', date: '04/01/2020' },
-    { version: 'v1.4.0-RC5', date: '03/02/2020' },
-    { version: 'v1.4.0-RC4', date: '02/10/2020' },
-    { version: 'v1.4.0-RC3', date: '01/21/2020' },
-    { version: 'v1.4.0-RC2', date: '01/01/2020' },
-    { version: 'v1.4.3-RC1', date: '12/25/2019' },
-    { version: 'v1.3.9-RELEASE', date: '11/15/2019' },
-  ];
-
-  //go to project dashboard
+  // go to project dashboard
   gotoProject() {
     const entityId = this.route.snapshot.paramMap.get('entityId');
     const url = "dashboard/entity/" + entityId + "/project/" + this.projectId;
     this.router.navigate([url]);
   }
 
-  //go to License details page
+  // go to License details page
   gotoLicense(liId) {
     const entityId = this.route.snapshot.paramMap.get('entityId');
     const scanId = this.route.snapshot.paramMap.get('scanId');
@@ -157,12 +143,12 @@ export class ScanAssetDetailComponent implements OnInit {
     this.router.navigate([url]);
   }
 
-  //Initialize breadcum details
+  // Initialize breadcum details
   private initBreadcum() {
     this.breadcumDetail = this.projectBreadcumsService.getProjectBreadcum();
   }
 
-  //open Attribution process model
+  // open Attribution process model
   openAttributionModel(content) {
     if (!!this.selectedMatches && this.selectedMatches.length >= 1) {
       content.show();
