39b5e7db67908475bf9cb846356d493822eceeae
Gaurav Thakkar <gthakkar.it@gmail.com>
2021-12-30 19:11:43 +0530

    https://github.com/threatrix/product/issues/1138

--- src/app/threat-center/dashboard/project/dashboard-top-bar/top-bar.component.ts
+++ src/app/threat-center/dashboard/project/dashboard-top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
 import { Project } from "@app/models";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Observable } from "rxjs";
@@ -14,9 +14,13 @@ export class ProjectDashboardTopbarComponent implements OnInit, OnDestroy {
     @Input() set obsProject(val: Observable<Project>) {
         this.initMostRecentScan(val);
     }
+
+    @Output() openScanTab: EventEmitter<any> = new EventEmitter();
+
     mostRecentScan;
     errorMsg: string;
     log: string;
+    totalScanCount = 0;
 
     constructor(private modalService: NgbModal) {
     }
@@ -28,12 +32,16 @@ export class ProjectDashboardTopbarComponent implements OnInit, OnDestroy {
     initMostRecentScan(val: Observable<Project>) {
         if (val) {
             val.subscribe(res => {
+                this.totalScanCount = res.scans.totalCount;
                 if (res && res.scans.edges.length >= 1) {
                     this.mostRecentScan = res.scans.edges.reduce((a: any, b: any) => (a.node.created > b.node.created ? a : b));
                 }
-                console.log(this.mostRecentScan);
             });
         }
+    }
+
+    updateData(scan){
+        this.mostRecentScan = scan;
     }
 
     openLogs(content, errorMsg: string, log: string) {
@@ -41,6 +49,10 @@ export class ProjectDashboardTopbarComponent implements OnInit, OnDestroy {
         this.errorMsg = errorMsg;
         this.log = log;
         this.modalService.open(content, { windowClass: 'md-class', centered: true });
+    }
+
+    openScanTable() {
+        this.openScanTab.emit('');
     }
 
 }
