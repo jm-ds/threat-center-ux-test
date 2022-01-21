d7c5f19a30e73c05f8c3d15bb7dc94a162239285
Gaurav Thakkar <gthakkar.it@gmail.com>
2022-01-21 16:05:36 +0530

    https://github.com/threatrix/product/issues/913

---
+++ src/app/security/github-error-page/github-error-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Component({
+    selector: 'git-hub-error',
+    templateUrl: './github-error-page.component.html',
+    styleUrls: ['./github-error-page.component.scss']
+})
+
+export class GithubErrorComponent implements OnInit {
+
+    gitHubErrorMessage = {};
+    constructor(private route: ActivatedRoute, private router: Router) {
+        this.route.queryParams
+            .subscribe(params => {
+                if (!!params) {
+                    this.gitHubErrorMessage = params;
+                }
+            });
+    }
+    ngOnInit(): void {
+
+    }
+    goBack() {
+        this.router.navigate(['/login']);
+    }
+}
