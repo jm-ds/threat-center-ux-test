a14358fdb938794839f2cc43bd7d647f1fd292b7
A-STAR <andreysuperstar@gmail.com>
2022-05-22 20:39:14 +0300

    #1270 feat: add GraphQL explorer script
    Add GraphQL explorer script on API explorer page.
    
    Remove script while leaving the page.

--- src/app/admin/api-explorer/api-explorer.component.ts
+++ src/app/admin/api-explorer/api-explorer.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-api-explorer',
   templateUrl: './api-explorer.component.html',
   styleUrls: ['./api-explorer.component.scss']
 })
-export class ApiExplorerComponent { }
+export class ApiExplorerComponent implements OnDestroy {
+  private scriptEl: HTMLScriptElement;
+
+  constructor() {
+    this.addGraphQLExplorerScript();
+  }
+
+  private addGraphQLExplorerScript() {
+    const headEl = document.querySelector('head');
+
+    this.scriptEl = document.createElement('script');
+
+    this.scriptEl.src = 'https://embeddable-explorer.cdn.apollographql.com/_latest/embeddable-explorer.umd.production.min.js';
+
+    headEl.append(this.scriptEl);
+  }
+
+  ngOnDestroy() {
+    this.scriptEl.remove();
+  }
+}
